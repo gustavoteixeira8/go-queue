14b048b680d67aea55034a2c93630c82782af8fc
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2022-12-15 11:46:23 -0300

    add first version

---
+++ queue.go
@@ -0,0 +1,210 @@
+package queue
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"reflect"
+	"sync"
+	"time"
+
+	"github.com/go-redis/redis"
+)
+
+type QueueProtocol[T any] interface {
+	Enqueue(args T) error
+	Listen(callback CallbackFunc[T])
+	Dequeue(callback CallbackFunc[T]) error
+}
+
+// Caso uma callback retorne um erro, essa estrutura de dados será salva no redis
+// com informações sobre o erro, a data em que o error ocorreu e os dados que estavam na fila.
+type QueueError[T any] struct {
+	Data         T
+	ErrorMessage string
+	CreatedAt    time.Time
+}
+
+type CallbackFunc[T any] func(args T) error
+
+type QueueConfig struct {
+	// Número de vezes que uma callback será reexecutada em caso de erro
+	Retries int
+
+	// Tempo de espera para executar a callback em caso de erro
+	RetriesAfter time.Duration
+}
+
+type Queue[T any] struct {
+	redis  *redis.Client
+	config *QueueConfig
+	name   string
+}
+
+// Salva os dados de erro no redis.
+func (q *Queue[T]) setQueueError(queueError QueueError[T]) error {
+	var err error
+
+	queueErrorName := fmt.Sprintf("%s:errors", q.name)
+
+	queueErrors := []QueueError[T]{}
+	var errorsBytes []byte
+
+	resp := q.redis.Get(queueErrorName)
+
+	errorsBytes, _ = resp.Bytes()
+
+	if len(errorsBytes) != 0 {
+		err = json.Unmarshal(errorsBytes, &queueErrors)
+
+		log.Println(err)
+	}
+
+	queueErrors = append(queueErrors, queueError)
+
+	errorsBytes, err = json.Marshal(queueErrors)
+
+	if err != nil {
+		return err
+	}
+
+	respSet := q.redis.Set(queueErrorName, string(errorsBytes), 0)
+
+	return respSet.Err()
+}
+
+// Adiciona um novo valor no final da fila.
+func (q *Queue[T]) Enqueue(args T) error {
+	var valueToAdd interface{} = args
+
+	isStruct := reflect.ValueOf(args).Kind() == reflect.Struct
+	isMap := reflect.ValueOf(args).Kind() == reflect.Map
+	isSlice := reflect.ValueOf(args).Kind() == reflect.Slice
+
+	if isSlice || isMap || isStruct {
+		argsBytes, err := json.Marshal(args)
+		if err != nil {
+			return err
+		}
+		valueToAdd = string(argsBytes)
+	}
+
+	resp := q.redis.RPush(q.name, valueToAdd)
+	return resp.Err()
+}
+
+// Desenfileira um item.
+// Executa a callback passando os dados encontrados na primeira posição da fila.
+func (q *Queue[T]) Dequeue(callback CallbackFunc[T]) error {
+	var (
+		args T
+		err  error
+	)
+
+	resp := q.redis.BLPop(0, q.name)
+
+	if resp.Err() != nil {
+		return resp.Err()
+	}
+
+	respString := resp.Val()[1]
+
+	isStruct := reflect.ValueOf(args).Kind() == reflect.Struct
+	isMap := reflect.ValueOf(args).Kind() == reflect.Map
+	isSlice := reflect.ValueOf(args).Kind() == reflect.Slice
+
+	if isMap || isStruct || isSlice {
+		err = json.Unmarshal([]byte(respString), &args)
+		if err != nil {
+			return err
+		}
+	}
+
+	runSuccessfully := false
+
+	for i := 0; i < q.config.Retries; i++ {
+		err = callback(args)
+
+		if err == nil {
+			runSuccessfully = true
+			break
+		}
+
+		log.Printf("Retrying to execute callback (%d)\n", i+1)
+		time.Sleep(q.config.RetriesAfter)
+	}
+
+	if !runSuccessfully {
+		qError := QueueError[T]{
+			Data:         args,
+			ErrorMessage: err.Error(),
+			CreatedAt:    time.Now(),
+		}
+
+		err = q.setQueueError(qError)
+
+		if err != nil {
+			log.Println(err)
+		}
+	}
+
+	return err
+}
+
+// Verifica se existe algum item na fila a cada 10 segundos.
+// Se existir, essa função chamará a função Queue.Dequeue(callback CallbackFunc[T])
+func (q *Queue[T]) Listen(callback CallbackFunc[T]) {
+	log.Printf("Running Listen from queue: %s\n", q.name)
+
+	var err error
+
+	for {
+		for q.redis.LLen(q.name).Val() != 0 {
+			err = q.Dequeue(callback)
+			if err != nil {
+				log.Println(err)
+				break
+			}
+			time.Sleep(time.Second / 2)
+		}
+		time.Sleep(time.Second * 10)
+	}
+}
+
+type ProcessableFunc func(wg *sync.WaitGroup)
+
+// Executa funções de forma assíncrona usando WaitGroup para sincronizar as Goroutines.
+// As funções passadas por parâmetro recebem um ponteiro para o WaitGroup e devem executar
+// WaitGroup.Done() quando finalizarem sua execução
+func ProcessAsync(callbacks ...ProcessableFunc) {
+	var wg sync.WaitGroup
+
+	wg.Add(len(callbacks))
+
+	for _, callback := range callbacks {
+		go callback(&wg)
+	}
+
+	wg.Wait()
+}
+
+// Cria uma nova instância da fila.
+func NewQueue[T any](qName string, config *QueueConfig, redisClient *redis.Client) (QueueProtocol[T], error) {
+	// Ping no redis para garantir que a conexão existe
+	if ping := redisClient.Ping(); ping.Err() != nil {
+		return nil, ping.Err()
+	}
+
+	if config == nil {
+		config = &QueueConfig{
+			Retries:      1,
+			RetriesAfter: time.Second,
+		}
+	}
+
+	return &Queue[T]{
+		name:   qName,
+		redis:  redisClient,
+		config: config,
+	}, nil
+}
---
+++ test/main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/go-redis/redis"
+	queue "github.com/gustavoteixeira8/go-queue"
+)
+
+type MailOpts struct {
+	To []string
+}
+
+type User struct {
+	Name string
+}
+
+func NewMailQueue() queue.QueueProtocol[MailOpts] {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	mailQueue, err := queue.NewQueue[MailOpts]("mail-queue", nil, rdb)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return mailQueue
+}
+
+func NewUserQueue() queue.QueueProtocol[User] {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	userQueue, err := queue.NewQueue[User]("user-queue", nil, rdb)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return userQueue
+}
+
+func RunMailQueue(wg *sync.WaitGroup) {
+	qMail := NewMailQueue()
+
+	callbackMail := func(args MailOpts) error {
+		fmt.Printf("Sending email to %s\n", args.To)
+
+		return nil
+	}
+
+	qMail.Listen(callbackMail)
+
+	wg.Done()
+}
+
+func RunUserQueue(wg *sync.WaitGroup) {
+	qUser := NewUserQueue()
+
+	callbackUser := func(args User) error {
+		fmt.Printf("Hello %s\n", args.Name)
+
+		return nil
+	}
+
+	qUser.Listen(callbackUser)
+
+	wg.Done()
+}
+
+func AddValuesIntoRedis() {
+	// q := external.NewUserQueue()
+	qm := NewMailQueue()
+
+	for {
+		err := qm.Enqueue(MailOpts{To: []string{"gustavo@email.com"}})
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		err = qm.Enqueue(MailOpts{To: []string{"ana@email.com"}})
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		err = qm.Enqueue(MailOpts{To: []string{"iza@email.com"}})
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		err = qm.Enqueue(MailOpts{To: []string{"nai@email.com"}})
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		err = qm.Enqueue(MailOpts{To: []string{"tai@email.com"}})
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		err = qm.Enqueue(MailOpts{To: []string{"ju@email.com"}})
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		time.Sleep(time.Microsecond)
+	}
+}
+
+func ProcessValues() {
+	queue.ProcessAsync(RunMailQueue)
+}
+
+func main() {
+	// AddValuesIntoRedis()
+
+	// rdb := redis.NewClient(&redis.Options{
+	// 	Addr:     "localhost:6379",
+	// 	Password: "", // no password set
+	// 	DB:       0,  // use default DB
+	// })
+
+	// resp := rdb.Keys("mail-queue:*")
+
+	// fmt.Println(resp.Result())
+
+	ProcessValues()
+	// AddValuesIntoRedis()
+}
